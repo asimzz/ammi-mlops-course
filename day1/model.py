52e6c5179c1f242685aba97d0bc0bfbd97101e92
Asim Abdalla <asimabdalla99@gmail.com>
2024-09-08 18:10:18 +0000

    minor changes

--- day1/model.py
+++ day1/model.py
@@ -1,9 +1,9 @@
 from sklearn.linear_model import LogisticRegression
 
-def fit_model(X_train, y_train):
+def train(X_train, y_train):
     classifier = LogisticRegression().fit(X_train, y_train)
     return classifier
 
 def accuracy_score(classifier, X, y):
     score = classifier.score(X, y)
-    return round(score, 3) * 100
+    return score
